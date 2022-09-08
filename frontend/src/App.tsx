253475657e5807ead2b5b38eadd68bc62838d1c0
e-manfrin <rm87198@fiap.com>
2022-09-08 13:10:23 -0300

    Primeiro componente

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
-    <h1>Olá Emily</h1>
+    <>
+      <h1>Olá Emily</h1>
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
